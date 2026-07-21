7259c796477fc20b491655b7bc5583acdf44cf44
Adarsh Pandey <235998355+Adarsh24BDA70227@users.noreply.github.com>
2026-07-21 10:48:19 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.86%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,38 +1,20 @@
 class MyQueue:
-
     def __init__(self):
-        self.input = []
-        self.output = []
-        self.peekEl = -1
-        
+        self.s1 = []
+        self.s2 = []
 
-    def push(self, x: int) -> None:
-        if not self.input:
-            self.peekEl = x
-        self.input.append(x)    
-        
+    def push(self, x):
+        self.s1.append(x)
 
-    def pop(self) -> int:
-        if not self.output:
-            while self.input:
-                self.output.append(self.input.pop())
-        return self.output.pop()        
-        
+    def pop(self):
+        self.peek()
+        return self.s2.pop()
 
-    def peek(self) -> int:
-        if not self.output:
-            return self.peekEl
-        return self.output[-1]    
-        
+    def peek(self):
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]        
 
-    def empty(self) -> bool:
-        return not self.input and not self.output
-        
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+    def empty(self):
+        return not self.s1 and not self.s2
